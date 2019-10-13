69ad5024064b1f034e9872e7a438c64d3e1fd12b
eyabc <bey4314@naver.com>
2019-10-13 20:20:35 +0900

    -

---
+++ React-nodebird/ch_1/front/components/AppLayout.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from 'next/link'
+import { Menu, Input, Button } from 'antd'
+
+const AppLayout = ({children}) => {
+    return <div>
+        <Menu mode="horizontal">
+            <Menu.Item key="home"><Link href="/"><a>Nodebird</a></Link></Menu.Item>
+            <Menu.Item key="profile"><Link href="/profile"><a>프로필</a></Link></Menu.Item>
+            <Menu.Item key="search">
+                <Input.Search enterButton style={{ verticalAlign: 'middle'}}></Input.Search>
+            </Menu.Item>
+        </Menu>
+        <Link href="/signup"><a><Button>회원가입</Button></a></Link>
+        {children}
+    </div>
+}
+
+export default AppLayout
---
+++ React-nodebird/ch_1/front/pages/signup.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react'
+import { Form, Input, Checkbox, Button } from 'antd'
+
+const Signup = () => {
+    const [id, setId] = useState('')
+    const [nick, setNick] = useState('')
+    const [password, setPassword] = useState('')
+    const [passwordCheck, setPasswordCheck] = useState('')
+    const [term, setTerm] = useState('')
+    const [passwordError, setPasswordError] = useState(false)
+    const [termError, setTermError] = useState(false)
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (password !== passwordCheck) {
+            return setPasswordError(true)
+        }
+        if (!term) {
+            return setTermError(true)
+        }
+        console.log({
+            id,
+            nick,
+            password,
+            passwordCheck,
+            term
+        })
+    }
+    const onChangeId = (e) => {
+        setId(e.target.value)
+    }
+    const onChangeNick = (e) => {
+        setNick(e.target.value)
+
+    }
+    const onChangePassword = (e) => {
+        setPassword(e.target.value)
+        
+    }
+    const onChangePasswordChk = (e) => {
+        setPasswordError(e.target.value !== password)
+        setPasswordCheck(e.target.value)
+
+    }
+    const onChangeTerm = (e) => {
+        setTermError(false)
+        setTerm(e.target.checked)
+
+    }
+
+    return <>
+            <Form onSubmit={onSubmit} sytle={{padding: 10}}>
+                <div>
+                    <label htmlFor="user-id">아이디</label>
+                    <br />
+                    <Input name="user-id" value={id} required onChange={onChangeId} />
+                </div>
+                <div>
+                    <label htmlFor="user-id">닉네임</label>
+                    <br />
+                    <Input name="user-nick" value={nick} required onChange={onChangeNick} />
+                </div>
+                <div>
+                    <label htmlFor="user-password">비밀번호</label>
+                    <br />
+                    <Input name="user-password" value={password} required onChange={onChangePassword} />
+                </div>
+                <div>
+                    <label htmlFor="user-password-check">비밀번호 체크</label>
+                    <br />
+                    <Input name="user-password-check" value={passwordCheck} type="password" required onChange={onChangePasswordChk} />
+                    {passwordError && <div style={{ color: 'red' }}>비밀번호가 일치하지 않습니다.</div>}
+                </div>
+                <div>
+                    <Checkbox name="user-term" value={term} onChange={onChangeTerm}>개인정보동의</Checkbox>
+                    {termError && <div style={{ color: 'red'}}> 약관에 동의하셔야 합니다.</div>}
+                </div>
+                <div style={{ marginTop: 10}}></div>
+                <div>
+                    <Button type="primary" htmlType="submit">가입하기</Button>
+                </div>
+            </Form>
+    </>
+}
+
+export default Signup
